d9271bd4ee5ff284dbe5af43a84b41dcc530210f
Dev1 - filip <pelloiafilippo@live.com>
2023-06-26 10:37:12 +0200

    commit monday

---
+++ For.java
@@ -0,0 +1,11 @@
+public class For {
+    public static void main(String[] args){
+        String box[] = {"Flash", "Justice League 2017", "Birds of Prey"};
+
+
+        for (int index = 0; index < box.length; index++){
+            System.out.println(box[index]);
+        }
+
+    }
+}
